193a8ab79f626c1e40be221d07e75dc19b6276bf
Amanzhol <amanzholaimov@gmail.com>
2026-01-30 08:07:44 +0500

    Refactor map_renderer.h to simplify rendering functions
    Removed RenderSettings structure and RenderMapSvg function. Added RenderBusSvg function for rendering bus SVGs.

--- task1/map_renderer.h
+++ task1/map_renderer.h
@@ -1,33 +1,10 @@
+// map_renderer.h
 #pragma once
-#pragma message("### USING map_renderer.h from: " __FILE__)
-
-#include <iosfwd>
-#include <string_view>
+#include <string>
 
 #include "transport_catalogue.h"
 
-namespace tc::render {
-
-struct RenderSettings {
-    double width = 1600.0;
-    double height = 1000.0;
-    double padding = 100.0;
-
-    double stop_radius = 4.0;
-    double line_width = 6.0;
-
-    int label_font_size = 14;
-    double line_opacity = 0.7;
-
-    bool draw_stop_labels = true;
-    bool draw_bus_end_label = true;
-
-    std::string_view underlayer_color = "white";
-    double underlayer_width = 4.0;
-};
-
-void RenderMapSvg(const tc::catalogue::TransportCatalogue& db,
-                  std::ostream& out,
-                  const RenderSettings& settings = {});
-
-}  // namespace tc::render
+std::string RenderBusSvg(const Bus& bus,
+                         double width = 800.0,
+                         double height = 600.0,
+                         double padding = 50.0);
