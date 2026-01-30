89de838676ef856ad381ad6c92b694e4ceeb4be7
Amanzhol <amanzholaimov@gmail.com>
2026-01-30 08:18:35 +0500

    Create stat_reader.h

---
+++ task2/stat_reader.h
@@ -0,0 +1,10 @@
+// stat_reader.h
+#pragma once
+
+#include <iosfwd>
+#include <string_view>
+
+#include "transport_catalogue.h"
+
+void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request,
+                       std::ostream& output);
