d8632a11733dcb72674926d3dab40a2d31ca4107
Amanzhol <amanzholaimov@gmail.com>
2026-01-30 08:17:27 +0500

    Create map_renderer.h

---
+++ task2/map_renderer.h
@@ -0,0 +1,16 @@
+// map_renderer.h
+#pragma once
+#include <string>
+
+#include "transport_catalogue.h"
+
+std::string RenderBusSvg(const Bus& bus,
+                         double width = 800.0,
+                         double height = 600.0,
+                         double padding = 50.0);
+
+std::string RenderStopSvg(const Stop& stop,
+                          const std::vector<const Bus*>& buses,
+                          double width = 800.0,
+                          double height = 600.0,
+                          double padding = 50.0);
